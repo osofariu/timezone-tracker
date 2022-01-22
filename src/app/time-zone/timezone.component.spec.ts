c0e4e32f5ab40e437cbed04a95c0b2677c099b3d
Ovi Sofariu <ovi@sofariu.com>
2022-01-22 15:21:28 -0500

    add luxon, show local time for card

--- src/app/time-zone/timezone.component.spec.ts
+++ src/app/time-zone/timezone.component.spec.ts
@@ -1,5 +1,6 @@
 import {ComponentFixture, TestBed, waitForAsync} from "@angular/core/testing"
 import {TimeZoneComponent} from "./timezone.component"
+import {DateTime, Settings} from "luxon"
 
 describe('TimeZone Component',  () => {
   let fixture: ComponentFixture<TimeZoneComponent>
@@ -11,6 +12,8 @@ describe('TimeZone Component',  () => {
     })
     fixture = TestBed.createComponent(TimeZoneComponent);
     app = fixture.componentInstance;
+
+    Settings.now = () => 1642882500000
   }))
 
   it('displays the timezone', () => {
@@ -19,7 +22,14 @@ describe('TimeZone Component',  () => {
     fixture.detectChanges()
 
     let timeZoneElement = fixture.nativeElement.querySelector('.timezone-name')
-    console.log('*** ', timeZoneElement)
     expect(timeZoneElement.textContent).toContain(fakeTimeZoneName)
+  })
+
+  it('displays localtime in desired format', () => {
+    app.timezone = 'America/New_York'
+    fixture.detectChanges()
+
+    let dateTimeElement = fixture.nativeElement.querySelector('.date-time')
+    expect(dateTimeElement.textContent).toEqual('1/22/2022, 3:15 PM')
   })
 })
--- src/app/time-zone/timezone.component.ts
+++ src/app/time-zone/timezone.component.ts
@@ -1,9 +1,18 @@
-import {Component, Input} from "@angular/core";
+import {Component, Input, OnInit} from "@angular/core"
+import {DateTime, Settings} from 'luxon'
 
 @Component({
   selector: 'app-timezone',
   templateUrl: './timezone.component.html'
 })
-export class TimeZoneComponent {
+export class TimeZoneComponent implements OnInit{
   @Input() timezone?: string;
+  localTime: string = ''
+
+  ngOnInit(): void {
+    if (this.timezone) {
+      const localTimeDate = DateTime.local().setZone(this.timezone)
+      this.localTime = localTimeDate.toLocaleString(DateTime.DATETIME_SHORT)
+    }
+  }
 }
